883e3e8861df64871ff3a36a0e1b6da45a773227
Tristan Wong <tristanwong321@hotmail.com>
2022-09-25 23:31:02 -0400

    added geolocation API to find user city

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
     'apiKey': "9725cf1052914fa241258e05169938b2",
     fetchWeather: function (city) {
         fetch(
-            'https://api.openweathermap.org/data/2.5/weather?q=' 
+            'https://api.openweathermap.org/data/2.5/weather?q='
             + city 
             + '&units=metric&appid=' 
             + this.apiKey
@@ -28,6 +28,65 @@ let weather = {
     }
 };
 
+let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+        var api_key = 'bfbcb09911ec44b993f60c6dcfce83b1';
+
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+
+        var request_url = api_url
+            + '?'
+            + 'key=' + api_key
+            + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+            + '&pretty=1'
+            + '&no_annotations=1';
+
+        // see full list of required and optional parameters:
+        // https://opencagedata.com/api#forward
+
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+
+        request.onload = function() {
+            // see full list of possible response codes:
+            // https://opencagedata.com/api#codes
+
+            if (request.status === 200){
+            // Success!
+            var data = JSON.parse(request.responseText);
+            // display the location
+            weather.fetchWeather(data.results[0].components.city);
+
+            } else if (request.status <= 500){
+            // We reached our target server, but it returned an error
+
+            console.log("unable to geocode! Response code: " + request.status);
+            var data = JSON.parse(request.responseText);
+            console.log('error msg: ' + data.status.message);
+            } else {
+            console.log("server error");
+            }
+        };
+
+        request.onerror = function() {
+            // There was a connection error of some sort
+            console.log("unable to connect to server");
+        };
+
+        request.send();  // make the request
+    },
+    getLocation: function() {
+        function success(data){
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+        }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, console.error);
+        } else {
+            weather.fetchWeather('Toronto');
+        }
+    }
+ }
+
 document.querySelector('.search button').addEventListener('click', function() {
     weather.search();
 });
@@ -38,4 +97,4 @@ document.querySelector('.search-bar').addEventListener('keyup', function(event) 
     }
 });
 
-weather.fetchWeather('Toronto');
+geocode.getLocation();
